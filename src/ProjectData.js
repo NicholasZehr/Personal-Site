9a9b0ffa579925c93e9f2167481e4826d7defa49
Nicholas Zehr <nicholas.zehr@icloud.com>
2022-07-19 09:37:14 -0500

    Preparing to make carousel

---
+++ src/ProjectData.js
@@ -0,0 +1,29 @@
+const projectData = [
+    {
+        name: "Grace Shopper",
+        description: "Fullstack Academy's exercise full stack application comprehension and understanding.",
+        techUsed: [], 
+        //projectLength is in working days
+        projectLength: 0,
+        deployedURL: "",
+        githubURL:""
+    },
+    {
+        name: "Sytime",
+        description: "Submission for the 2021 EthGlobal Web3 Jam Hackathon",
+        techUsed: [], 
+        //projectLength is in working days
+        projectLength: 0,
+        deployedURL: "",
+        githubURL:""
+    },
+    {
+        name: "Percolate",
+        description: "Fullstack Academy's exercise in product design, planning, and leveraging new technologies outside of the PERN stack.",
+        techUsed: [], 
+        //projectLength is in working days
+        projectLength: 0,
+        deployedURL: "",
+        githubURL:""
+    }
+]
