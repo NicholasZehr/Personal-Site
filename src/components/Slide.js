b48217af58cc1b08169cb407d631769d74873c42
Nicholas Zehr <nicholas.zehr@icloud.com>
2022-07-30 10:29:11 -0500

    Fading on seperate carousel slides working

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Slide = (props) => {
     const{project, active} = props
@@ -7,18 +7,15 @@ const Slide = (props) => {
     useEffect(() => {
         setTransit(false)
         setTimeout(() => {
-            if (!active) {
-                console.log("run the transit thing")
                 setTransit(true)
-            }
         }, 500);
-    }, [name, active])
+    }, [ active, name])
    
 return (
     <div className={`slide box ${active ? 'active' : transit ? 'inactive-after': 'inactive-before'}`} >{`This is your Slide Component`} 
         <div>{name}</div>
         <div>{description}</div>
-        <img className="fade-in-image" src={photoURL} alt="Project" title="source: imgur.com" />
+        <img className={`${active && !transit ? 'fade-in-image' : ''}`} src={photoURL} alt="Project" title="source: imgur.com" />
     </div>
 )}
 
