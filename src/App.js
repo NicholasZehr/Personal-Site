412eaf4a0a6bf9c368fdb1e3ec8e34b5f344397d
Nicholas Zehr <nicholas.zehr@icloud.com>
2022-08-10 18:45:41 -0500

    installed react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import { NavBar, AboutMe, ProjectPane } from "./components";
+import Contact from "./components/Contact";
 function App() {
   return (
     <div id="app">
       <NavBar />
-      <AboutMe />
-      <ProjectPane />
+      <Routes>
+        <Route path="about" element={<AboutMe />}>About Me </Route>
+        <Route path="projects" element={ <ProjectPane />}>About Me </Route>
+      </Routes >
+      <Contact />
     </div>
   );
 }
--- src/components/ProjectPane.jsx
+++ src/components/ProjectPane.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { projectData as projects } from "../ProjectData";
 import { Slide } from "./";
+import Contact from "./Contact";
 export const ProjectPane = () => {
   const [activeSlide, setActiveSlide] = useState(0);
   const [leftSlide, setLeftSlide] = useState(projects.length - 1);
@@ -37,7 +38,6 @@ export const ProjectPane = () => {
 
   return (
     <div className="box project-pane">
-      {`This is your ProjectPane Component`}
       <button onClick={reverseClick}>Previous</button>
       <div className="presented-slides">
         <Slide project={projects[leftSlide]} />
