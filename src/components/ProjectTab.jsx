b76cc867d9dbb3846f83bb03e415ba8f768ddb5f
Nicholas Zehr <nicholas.zehr@icloud.com>
2022-08-10 23:05:47 -0500

    Computer battery is low. will be closing this branch

--- src/components/ProjectTab.jsx
+++ src/components/ProjectTab.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 const ProjectTab = (props) => {
-return (
-    <div className='project-tab'>
-        <button className="project-tab-button">Projects</button>
-        <Link className="project-tab-link" to="projects/personal">Personal Projects</Link>
-        <Link className="project-tab-link" to="projects/student">Student Work</Link>
-        <Link className="project-tab-link" to="projects/coworker">Co-Worker Flexes</Link>
- </div>
-)}
+  return (
+    <div className="project-tab">
+      <button className="project-tab-button">Projects</button>
+      <div className="project-tab-content">
+        <Link className="project-tab-link" to="projects/personal">
+          Personal Projects
+        </Link>
+        <Link className="project-tab-link" to="projects/student">
+          Student Work
+        </Link>
+        <Link className="project-tab-link" to="projects/coworker">
+          Co-Worker Flexes
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default ProjectTab
+export default ProjectTab;
